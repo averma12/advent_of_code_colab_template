e8c1a056af0e9fb0fbdcb7fb380c51fd5a5457ec
Abhinav Verma <verma.abhinav275@gmail.com>
2025-01-10 07:37:28 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ setup(
         'ipython',
         'advent-of-code-data',  # aocd package
     ],
-    author="Your Name",
-    author_email="your.email@example.com",
+    author="Abhinav Verma",
+    author_email="verma.abhinav275l@gmail.com",
     description="A template generator for Advent of Code solutions in Google Colab",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/aoc-colab-template",
+    url="https://github.com/averma12/advent_of_code-colab-template",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
